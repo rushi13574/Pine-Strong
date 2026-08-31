41eb32b6bd23237a7a25d93dcba120f753bcaa3b
rushi13574 <188558779+rushi13574@users.noreply.github.com>
2026-08-31 23:45:15 +0530

    error update

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,23 +1,27 @@
-import express, { type Express } from "express";
+import express, { type Express, type Request, type Response } from "express";
 import cors from "cors";
-import pinoHttp from "pino-http";
+import pinoHttpModule from "pino-http";
 import router from "./routes";
 import { logger } from "./lib/logger";
 
 const app: Express = express();
 
+const pinoHttp = (typeof pinoHttpModule === "function"
+  ? pinoHttpModule
+  : (pinoHttpModule as any).default || (pinoHttpModule as any).pinoHttp) as unknown as typeof pinoHttpModule;
+
 app.use(
-  pinoHttp({
+  (pinoHttp as any)({
     logger,
     serializers: {
-      req(req) {
+      req(req: any) {
         return {
           id: req.id,
           method: req.method,
           url: req.url?.split("?")[0],
         };
       },
-      res(res) {
+      res(res: any) {
         return {
           statusCode: res.statusCode,
         };
